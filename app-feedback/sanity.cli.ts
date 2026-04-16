7273f5fe7ea7a7784afe007a14a6d890fdaabae9
Espen Pedersen <espen.pedersen@tieto.com>
2026-04-16 16:22:36 +0200

    Initial

---
+++ app-feedback/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  app: {
+    organizationId: 'ozRs8p6ZW',
+    entry: './src/App.tsx',
+  },
+})
---
+++ app-feedback/src/App.tsx
@@ -0,0 +1,35 @@
+import { type SanityConfig } from "@sanity/sdk";
+import { SanityApp } from "@sanity/sdk-react";
+import { Flex, Spinner } from "@sanity/ui";
+import { SanityUI } from "./SanityUI";
+import { Feedback } from "./Feedback";
+import { FeedbackEvents } from "./FeedbackEvents";
+
+function App() {
+  // apps can access many different projects or other sources of data
+  const sanityConfigs: SanityConfig[] = [
+    {
+      projectId: "so39vnkw",
+      dataset: "production",
+    },
+  ];
+
+  function Loading() {
+    return (
+      <Flex justify="center" align="center" width="100vw" height="fill">
+        <Spinner />
+      </Flex>
+    );
+  }
+
+  return (
+    <SanityUI>
+      <SanityApp config={sanityConfigs} fallback={<Loading />}>
+        <Feedback />
+        <FeedbackEvents />
+      </SanityApp>
+    </SanityUI>
+  );
+}
+
+export default App;
